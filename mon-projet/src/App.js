445d59b2a44c5b7033a11cf3c98ca12b0f3b8ae5
mohamedbesrour <118281616+mohamedbesrour@users.noreply.github.com>
2023-10-05 14:10:11 +0200

    sécurité api key avec .env

--- mon-projet/src/App.js
+++ mon-projet/src/App.js
@@ -1,65 +1,76 @@
-import React, {useState} from "react";
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
-const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1f16c45b94420ed609717675e79923f5` 
-// const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1f16c45b94420ed609717675e79923f5`
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
 
-const searchLocation = (event) => {
-  if (event.key === 'Enter') {
-    axios.get(url).then((response) => {
-      setData(response.data)
-      console.log(response.data)
-    })
-    setLocation('')
-  }
-}
+  // const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=608f4cfb0397a22ff2827e8badf0cb0a`
+  // const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1f16c45b94420ed609717675e79923f5`
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => console.log(error));
+      setLocation("");
+    }
+  };
+  console.log(url);
 
   return (
     <div className="app">
-    <div className="search">
-      <input
-        value={location}
-        onChange={event => setLocation(event.target.value)}
-        onKeyPress={searchLocation}
-        placeholder='Enter Location'
-        type="text" />
-    </div>
-    <div className="container">
-      <div className="top">
-        <div className="location">
-          <p>{data.name}</p>
-        </div>
-        <div className="temp">
-          {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-        </div>
-        <div className="description">
-          {data.weather ? <p>{data.weather[0].main}</p> : null}
-        </div>
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
       </div>
-
-      {data.name !== undefined &&
-        <div className="bottom">
-          <div className="feels">
-            {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-            <p>Température ressentie</p>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
           </div>
-          <div className="humidity">
-            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-            <p>Humidité</p>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className="wind">
-            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-            <p>Vent</p>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-      }
 
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              ) : null}
+              <p>Température ressentie</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidité</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Vent</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
   );
 }
 
